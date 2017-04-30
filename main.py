b060366bb312a73221308f76ec1f240ce47dcf22
lagzles <leandro.arns@gmail.com>
2017-04-29 21:05:09 -0300

    ajuste para criação do db

--- main.py
+++ main.py
@@ -1,14 +1,13 @@
 import vigas_database as data
 
 beam_name = input('Nome da viga:')
-height = 40
-width = 20
-length = 250
-
-load = 500
+height = int(input('Altura da viga:'))
+width = int(input('Largura da viga:'))
+length = int(input('Vão da viga:'))
+load = int(input('Carga atuante:'))
 
 data.create_table()
-data.data_entry(beam_name, height, width, length, load)
+data.data_entry(beam_name, height, width, length, load, 0, 0, 0)
 
 
 
--- vigas_database.py
+++ vigas_database.py
@@ -12,10 +12,10 @@ def create_table():
               ' shear_armor REAL)')
 
 def data_entry(name, height, width, length, load, positive_armor, negative_armor, shear_armor):
-    c.execute("INSERT INTO stuffToPlot VALUES("
-              "%(1)s,%(2)s,%(3)s,%(4)s,%(5)s,%(6)s,%(7)s,%(8)s)"
-              % {'1': name, '2': height, '3': width, '4': length, '5': load,
-                 '6': positive_armor, '7': negative_armor, '8': shear_armor})
+    c.execute("INSERT INTO iso_beams "
+              "(beam_name, height, width, length, load, positive_armor, negative_armor, shear_armor)"
+              "VALUES (?, ?, ?, ?, ?, ?, ?, ?)",
+              (name, height, width, length, load, positive_armor, negative_armor, shear_armor))
     conn.commit()
     c.close()
     conn.close()
