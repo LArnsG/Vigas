f83e296595477761feaed82576f0d1ec9bd54446
lagzles <leandro.arns@gmail.com>
2017-05-02 15:32:58 -0300

    definição do retorno da funçao de analise isostatica

--- strutural_analysis.py
+++ strutural_analysis.py
@@ -2,4 +2,8 @@ import math
 
 def def_isostatic_stress(beam_length, beam_load):
     shear_stress = beam_length * beam_load / 2
-    positive_moment = (beam_load * beam_length ** 2) / 8
+    positive_moment = (beam_load * math.pow(beam_length, 2)) / 8
+
+    return {'shear': shear_stress, 'positive_moment': positive_moment}
+
+
