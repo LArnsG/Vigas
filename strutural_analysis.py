3698644f08eabb62039269aae65319795b849f47
lagzles <leandro.arns@gmail.com>
2017-05-02 15:39:52 -0300

    Função de taxa maxima de armadura e minima

--- strutural_analysis.py
+++ strutural_analysis.py
@@ -7,3 +7,12 @@ def def_isostatic_stress(beam_length, beam_load):
     return {'shear': shear_stress, 'positive_moment': positive_moment}
 
 
+def minimum_steel_area(height, width):
+    concrete_area = height * width
+    as_minimum = 0.0015 * concrete_area
+    as_maximum = 0.04 * concrete_area
+    return {'min': as_minimum, 'max': as_maximum}
+
+
+
+
